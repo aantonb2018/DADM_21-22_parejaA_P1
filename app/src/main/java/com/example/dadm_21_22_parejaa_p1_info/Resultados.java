acc5d8d0f5e2b5504a9ed529d4d7663b648eb621
miniponti <61016923+miniponti@users.noreply.github.com>
2021-10-09 20:22:24 +0200

    mejora pantalla inicial

---
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/Resultados.java
@@ -0,0 +1,14 @@
+package com.example.dadm_21_22_parejaa_p1_info;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Resultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultados);
+    }
+}
