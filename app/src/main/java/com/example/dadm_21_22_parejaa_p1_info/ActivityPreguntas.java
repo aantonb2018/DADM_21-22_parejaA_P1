ddb1fb7f3c9eb1d16d59844ad1a967bd2b2a1690
miniponti <61016923+miniponti@users.noreply.github.com>
2021-10-10 17:10:24 +0200

    si el usuario no introduce nombre no se avanza

--- app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/ActivityPreguntas.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/ActivityPreguntas.java
@@ -1,17 +1,13 @@
 package com.example.dadm_21_22_parejaa_p1_info;
 
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class ActivityPreguntas extends AppCompatActivity {
@@ -33,7 +29,7 @@ public class ActivityPreguntas extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_preguntas);
 
         leerItems(); //Metodo para leer los items de string.xml y poder usarlos en el MainActivity
 
--- app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/MenuPrincipal.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/MenuPrincipal.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MenuPrincipal extends AppCompatActivity {
 
@@ -17,22 +18,28 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        getSupportActionBar().hide();
 
-//        inputTexto = (EditText) findViewById(R.id.txt_nombre);
-//        btn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (TextUtils.isEmpty(inputTexto.getText().toString())){
-//                    Toast.makeText(MenuPrincipal.this,"Introduce un nombre", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
+        // Inicializacion input y botón
+        inputTexto = (EditText) findViewById(R.id.txt_nombre);
+        btn = (Button) findViewById(R.id.btn_empezar);
+
+        // Cuando se pulsa sobre el botón "empezar" se llama a este evento
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // si el campo de input está vacío se lanza un mensaje pidiendo el nombre
+                if ((inputTexto.getText().toString()).isEmpty()){
+                    Toast.makeText(MenuPrincipal.this,"Introduce un nombre", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    // en caso contrario, se pasa a la siguiente actividad
+                    Intent empezar = new Intent(MenuPrincipal.this, ActivityPreguntas.class);
+                    startActivity(empezar);
+                }
+            }
+        });
     }
 
-    //Metodo para el boton empezar
-    public void empezar(View view){
-        Intent empezar = new Intent(this, ActivityPreguntas.class);
-        startActivity(empezar);
-
-    }
 }
