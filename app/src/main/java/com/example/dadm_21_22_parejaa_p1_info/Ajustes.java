ceabe5eace4992b519db59037b490bd236a00932
miniponti <eleponti.m@outlook.com>
2021-10-26 09:42:19 +0200

    modificacion activity ajustes

--- app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/Ajustes.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/Ajustes.java
@@ -3,8 +3,11 @@ package com.example.dadm_21_22_parejaa_p1_info;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class Ajustes extends AppCompatActivity {
+
+    SeekBar barra_volumen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
