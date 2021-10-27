14877bbc82bc29dd90e55bb496705b5e1220fe8a
miniponti <eleponti.m@outlook.com>
2021-10-27 10:15:50 +0200

    ya no peta

--- app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/Ajustes.java
+++ app/src/main/java/com/example/dadm_21_22_parejaa_p1_info/Ajustes.java
@@ -3,15 +3,20 @@ package com.example.dadm_21_22_parejaa_p1_info;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class Ajustes extends AppCompatActivity {
 
     SeekBar barra_volumen;
     AudioManager audioManager;
+    Button btn_vol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,14 @@ public class Ajustes extends AppCompatActivity {
         getSupportActionBar().hide();
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        // #####################################################################
+        // BARRA VOLUMEN
+        // #####################################################################
+
         // encontrar el elemento seekBar de la activity y asignar a la variable
         barra_volumen = findViewById(R.id.barraVolumen);
 
-        // asignar al manager el audio del movil
+        // asignar al manager el audio del móvil
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         //obtener máximo volumen
@@ -34,7 +43,7 @@ public class Ajustes extends AppCompatActivity {
         // obtener volumen actual
         int actualVolumen = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-        // asignar maximo volumen a la barra de volumen
+        // asignar máximo volumen a la barra de volumen
         barra_volumen.setMax(maxVolumen);
 
         // asignar volumen actual a la barra
@@ -58,5 +67,24 @@ public class Ajustes extends AppCompatActivity {
             }
         });
 
+        // #####################################################################
+        // BOTON VOLVER AL MENU
+        // #####################################################################
+
+        // encontrar botón
+        btn_vol = (Button) findViewById(R.id.btn_volverMenu_resul);
+
+        // definición de evento onClick del botón "volver al menú"
+        btn_vol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // se pasa a la siguiente actividad
+                Intent empezar = new Intent(Ajustes.this, MenuPrincipal.class);
+
+                //empezar.putExtra("nick", (inputTexto.getText().toString()));
+                startActivity(empezar);
+            }
+        });
     }
 }
